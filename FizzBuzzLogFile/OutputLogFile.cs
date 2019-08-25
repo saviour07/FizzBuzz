f98e7e7e097ed99dc13db33225961f913d23b650
Carl Burnett <carlburnett07@gmail.com>
2019-08-25 17:28:55 +0100

    Add Output Log File
    Write to a text file in the user appdata directory
    Add fizzbuzz csproj which has been missing from all previous commits
    Update gitignore paths

---
+++ FizzBuzzLogFile/OutputLogFile.cs
@@ -0,0 +1,23 @@
+﻿using FizzBuzz;
+using System.IO;
+
+namespace FizzBuzzLogFile
+{
+    public class OutputLogFile : IOutputFizzBuzz
+    {
+        private readonly string _logFilePath;
+
+        public OutputLogFile(string logFilePath)
+        {
+            _logFilePath = logFilePath;
+        }
+
+        public void Output(string output)
+        {
+            using (var fs = new StreamWriter(_logFilePath, true))
+            {
+                fs.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ FizzBuzzLogFile/Program.cs
@@ -0,0 +1,14 @@
+﻿using FizzBuzz;
+using System;
+
+namespace FizzBuzzLogFile
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var outputLogFile = new OutputLogFile(Environment.ExpandEnvironmentVariables("%AppData%\\FizzBuzz.log"));
+            FizzBuzzRunner.Run(1000, outputLogFile);
+        }
+    }
+}
